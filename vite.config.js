d099bc6acc5fad8c0d7758011a95006893cd6b91
Tatiana <tanya18082000@mail.ru>
2024-04-13 22:35:53 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/react-tracking-scorm/",
-  root: "src",
   plugins: [react()],
   build: {
     outDir: "../dist",
